fa462d1d6711bc9685733ebcc2d18fdca52e5bb1
FrederikBA <frederikbandersen98@gmail.com>
2023-11-04 18:31:53 +0100

    delete repositories

--- MTOGO.Web/Interfaces/IRepository.cs
+++
@@ -1,6 +0,0 @@
-namespace MTOGO.Web.Interfaces;
-
-public interface IRepository<T> where T : IAggregateRoot
-{
-    // Implements IAggregateRoot because only entities should have repositories
-}
--- MTOGO.Web/Repositories/RepositoryBase.cs
+++
@@ -1,8 +0,0 @@
-using MTOGO.Web.Interfaces;
-
-namespace MTOGO.Web.Repositories;
-
-public class RepositoryBase<T> : IRepository<T> where T : IAggregateRoot
-{
-    // This is a generic repository meant to be used by all other repositories for basic CRUD operations
-}
