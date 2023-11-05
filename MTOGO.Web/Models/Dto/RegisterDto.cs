bbff83dd07b2f1bef7d87c861d01550ad97a495e
FrederikBA <frederikbandersen98@gmail.com>
2023-11-05 20:48:56 +0100

    add email validation to register dto

--- MTOGO.Web/Models/Dto/RegisterDto.cs
+++ MTOGO.Web/Models/Dto/RegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MTOGO.Web.Models.Dto;
 
 public class RegisterDto
 {
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
     public string Email { get; set; } = null!;
+    [Required(ErrorMessage = "Password is required")]
     public string Password { get; set; } = null!;
     public RoleTypes RoleType { get; set; }
 }
