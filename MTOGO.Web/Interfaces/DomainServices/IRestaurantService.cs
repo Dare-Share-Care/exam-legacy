01bcb67bad2f7077a11314b78d26570dc46ea028
FrederikBA <frederikbandersen98@gmail.com>
2023-11-25 11:23:16 +0100

    interface update

--- MTOGO.Web/Interfaces/DomainServices/IRestaurantService.cs
+++ MTOGO.Web/Interfaces/DomainServices/IRestaurantService.cs
@@ -5,11 +5,11 @@ namespace MTOGO.Web.Interfaces.DomainServices;
 
 public interface IRestaurantService
 {
-    public Task<RestaurantDto> CreateRestaurantAsync(string name);
-    public Task<List<MenuItemDto>> GetRestaurantMenuAsync(long restaurantId);
-    public Task<RestaurantDto> AddMenuItemAsync(long restaurantId, MenuItemDto dto);
-    public Task<RestaurantDto> RemoveMenuItemAsync(long restaurantId, long menuItemId);
+    Task<RestaurantDto> CreateRestaurantAsync(string name);
+    Task<List<MenuItemDto>> GetRestaurantMenuAsync(long restaurantId);
+    Task<RestaurantDto> AddMenuItemAsync(long restaurantId, MenuItemDto dto);
+    Task<RestaurantDto> RemoveMenuItemAsync(long restaurantId, long menuItemId);
     
-    public Task<List<RestaurantDto>> GetAllRestaurantsAsync();
+    Task<List<RestaurantDto>> GetAllRestaurantsAsync();
     
 }
